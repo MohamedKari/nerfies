a98c732c21f750c2c8594d4228bd007a2f8f9863
root <root@bolt-prcdwcmmqx-wqsk6u7eys.bolt-pods.turi-bolt.svc.int.usspk28.applecloud.io>
2022-07-13 07:48:15 +0000

    pdate

--- install-commands.sh
+++ install-commands.sh
@@ -1,9 +1,22 @@
-# nvidia-smi shows 11.2
+conda create --name nerfies python=3.8
 
+source deactivate
+conda activate nerfies
+which python
+
+pip install -r requirements.txt
+
+# nvidia-smi shows 11.2
 conda install -c conda-forge cudatoolkit=11.2.2
 conda install -c conda-forge cudnn=8.1.0.77
 pip install --upgrade pip
 
-pip install "jax[cuda11_cudnn805]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip uninstall jax
+pip install "jax[cuda11_cudnn805]==v0.3.13" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install tqdm
+
+# see requirements-full.txt for reference
+# pip freeze > requirements-full.txt
+
 
 python train.py --data_dir datasets/iphone-home --base_folder experiments/001 --gin_configs configs/test_vrig.gin
